5220ea3f577e2f9f9bb1768be7a7fc295e3b7f5d
amanbhoria <51995611+amanbhoria@users.noreply.github.com>
2023-01-14 22:44:11 +0530

    first commit

---
+++ src/components/FamilyDetails.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "../index.css";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Stack from "@mui/material/Stack";
+
+const FamilyDetails = (props) => {
+  return (
+    <div className="col-8">
+      <div className="card card-default" data-v-ce9038c6="">
+        <div className="card-header" data-v-ce9038c6="">
+          <div className="card-title" data-v-ce9038c6="">
+            Family Details
+          </div>
+        </div>
+        <div className="card-body" data-v-ce9038c6="">
+          <div className="card-body">
+            <div className="info">
+              <label htmlFor="">Name</label>
+              <label htmlFor="">Spouse:</label>
+              <label htmlFor="">Birth:</label>
+              <label htmlFor="">Location:</label>
+              <label htmlFor="">Present Address:</label>
+            </div>
+            <Stack direction="row" alignItems="center" spacing={2}>
+              <Button variant="contained" component="label">
+                Upload
+                <input hidden accept="image/*" multiple type="file" />
+              </Button>
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="label"
+              >
+                <input hidden accept="image/*" type="file" />
+                <PhotoCamera />
+              </IconButton>
+            </Stack>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FamilyDetails;
---
+++ src/components/Tree.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import FamilyDetails from "./FamilyDetails";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import FolderIcon from "@mui/icons-material/Folder";
+
+const Tree = () => {
+  const [open, setOpen] = useState(true);
+  const [property, setProperty] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const changeState = () => {
+    setProperty(true);
+  };
+
+  return (
+    <div className="row">
+      <div className="col-4">
+        <div class="card card-default" data-v-ce9038c6="">
+          <div class="card-header" data-v-ce9038c6="">
+            <div class="card-title" data-v-ce9038c6="">
+              Family Tree
+            </div>
+          </div>
+          <div className="card-body" data-v-ce9038c6="">
+            <div className="input-group mb-3">
+              <TextField
+                id="standard-basic"
+                type="text"
+                size="small"
+                className="form-control"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <Button type="submit" className="input-group-text">
+                <i class="fa-solid fa-magnifying-glass"></i>
+              </Button>
+            </div>
+
+            <List
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+            >
+              <ListItemButton onClick={changeState}>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Grand Father" />
+                <FamilyDetails isVisible={property}></FamilyDetails>
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemIcon>
+                  <FolderIcon></FolderIcon>
+                </ListItemIcon>
+                <ListItemText primary="Uncle" />
+              </ListItemButton>
+              <ListItemButton onClick={handleClick}>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary="Father" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Me" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </List>
+          </div>
+        </div>
+        <div className="functionalButton">
+          <Button type="submit" size="large" className="input-group-text">
+            Import Json
+          </Button>
+          <Button type="submit" size="large" className="input-group-text">
+            Add Family
+          </Button>
+          <Button type="submit" size="large" className="input-group-text">
+            Export JSON
+          </Button>
+          <Button type="submit" size="large" className="input-group-text">
+            Print Family Tree
+          </Button>
+        </div>
+      </div>
+      <FamilyDetails />
+    </div>
+  );
+};
+
+export default Tree;
